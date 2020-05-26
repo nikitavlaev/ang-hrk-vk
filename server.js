48c742e21befd2b417ade3058bbc0d7cdd9f2b60
Nikita <nikitavlaev00@gmail.com>
2020-05-26 22:32:28 +0300

    fix server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/vk-api-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/vk-api-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
